8f2a9c80c0493150b9743d1d24a77260e72902cf
Jaward Sesay <jkef96@icloud.com>
2022-08-27 21:44:38 +0800

    Update PlainTextEdit.h

--- FramelessWindow/PlainTextEdit.h
+++ FramelessWindow/PlainTextEdit.h
@@ -1,3 +1,4 @@
+
 #ifndef PLAINTEXTEDIT_H
 #define PLAINTEXTEDIT_H
 
@@ -5,18 +6,56 @@
 
 #include <QFont>
 #include <QPlainTextEdit>
+QT_BEGIN_NAMESPACE
+class QPaintEvent;
+class QResizeEvent;
+class QSize;
+class QWidget;
+QT_END_NAMESPACE
+
+class CodeLineArea;
+
 
 class PlainTextEdit : public QPlainTextEdit
 {
     Q_OBJECT
-public:
-    PlainTextEdit();
-    QFont codeFont;
 
-public slots:
-    void SendTextToFile();
-signals:
-    SendText(QString Text);//发送文本给文件保存函数
+public:
+    PlainTextEdit(QWidget *parent = 0);
+
+    void codeLineAreaPaintEvent(QPaintEvent *event);
+    int codeLineAreaWidth();
+
+protected:
+    void resizeEvent(QResizeEvent *event) override;
+
+private slots:
+    void updateCodeLineAreaWidth(int newBlockCount);
+    void highlightCurrentLine();
+    void updateCodeLineArea(const QRect &, int);
+
+private:
+    QWidget *codeLineArea;
 };
 
+// Code line Area & Width Definition
+class CodeLineArea : public QWidget
+{
+public:
+    CodeLineArea(PlainTextEdit *editor) : QWidget(editor) {
+        codeHighlighting = editor;
+    }
+
+    QSize sizeHint() const override {
+        return QSize(codeHighlighting->codeLineAreaWidth(), 0);
+    }
+
+protected:
+    void paintEvent(QPaintEvent *event) override {
+        codeHighlighting->codeLineAreaPaintEvent(event);
+    }
+
+private:
+    PlainTextEdit *codeHighlighting;
+};
 #endif // PLAINTEXTEDIT_H
