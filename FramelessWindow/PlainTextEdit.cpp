32c9571111c587984f5fb247ce1d61cae9b6460e
Jaward Sesay <jkef96@icloud.com>
2022-08-27 21:42:43 +0800

    Update PlainTextEdit.cpp

--- FramelessWindow/PlainTextEdit.cpp
+++ FramelessWindow/PlainTextEdit.cpp
@@ -1,14 +1,108 @@
 #include "PlainTextEdit.h"
+#include <QtWidgets>
 
-PlainTextEdit::PlainTextEdit()
+// CodeHighlighting Constructor
+PlainTextEdit::PlainTextEdit(QWidget *parent) : QPlainTextEdit(parent)
 {
-    QFont codeFont("Consolas", 12, 2);
-    this->setStyleSheet("background-color:rgb(255,255,255);border:none; border-radius:20px;color:rgb(93, 93, 94);"
-                            "selection-background-color:rgb(242, 239, 249);selection-color:rgb(147, 115, 238);"
-                            "padding: 20px;");
-    this->setFont(codeFont);
+
+    codeLineArea = new CodeLineArea(this);
+
+    connect(this, SIGNAL(blockCountChanged(int)), this, SLOT(updateCodeLineAreaWidth(int)));
+    connect(this, SIGNAL(updateRequest(QRect,int)), this, SLOT(updateCodeLineArea(QRect,int)));
+    connect(this, SIGNAL(cursorPositionChanged()), this, SLOT(highlightCurrentLine()));
+
+    updateCodeLineAreaWidth(0);
+    highlightCurrentLine();
+
 }
-void PlainTextEdit::SendTextToFile(){//编辑发送文本内容至文件
-    QString Text=this->toPlainText();
-    emit(SendText(Text));
+
+// codeLineAreaWidth Constructor
+int PlainTextEdit::codeLineAreaWidth()
+{
+    int digits = 1;
+    int max = qMax(1, blockCount());
+    while (max >= 10) {
+        max /= 10;
+        ++digits;
+    }
+
+    int space = 3 + fontMetrics().horizontalAdvance(QLatin1Char('9')) * digits;
+
+    return space;
+}
+
+
+void PlainTextEdit::updateCodeLineAreaWidth(int /* newBlockCount */)
+{
+    setViewportMargins(codeLineAreaWidth(), 0, 0, 0);
+}
+
+
+void PlainTextEdit::updateCodeLineArea(const QRect &rect, int dy)
+{
+    if (dy)
+        codeLineArea->scroll(0, dy);
+    else
+        codeLineArea->update(0, rect.y(), codeLineArea->width(), rect.height());
+
+    if (rect.contains(viewport()->rect()))
+        updateCodeLineAreaWidth(0);
+}
+
+
+void PlainTextEdit::resizeEvent(QResizeEvent *e)
+{
+    QPlainTextEdit::resizeEvent(e);
+
+    QRect cr = contentsRect();
+    codeLineArea->setGeometry(QRect(cr.left(), cr.top(), codeLineAreaWidth(), cr.height()));
+}
+
+// resizeEvent
+// cursorPositionChanged
+void PlainTextEdit::highlightCurrentLine()
+{
+    QList<QTextEdit::ExtraSelection> extraSelections;
+
+    if (!isReadOnly()) {
+        QTextEdit::ExtraSelection selection;
+
+        QColor lineColor = QColor(Qt::gray).lighter(145);
+
+        selection.format.setBackground(lineColor);
+        selection.format.setProperty(QTextFormat::FullWidthSelection, true);
+        selection.cursor = textCursor();
+        selection.cursor.clearSelection();
+        extraSelections.append(selection);
+    }
+
+    setExtraSelections(extraSelections);
+}
+
+
+void PlainTextEdit::codeLineAreaPaintEvent(QPaintEvent *event)
+{
+    QPainter painter(codeLineArea);
+    painter.fillRect(event->rect(), Qt::lightGray);
+
+
+    QTextBlock block = firstVisibleBlock();
+    int blockNumber = block.blockNumber();
+    int top = (int) blockBoundingGeometry(block).translated(contentOffset()).top();
+    int bottom = top + (int) blockBoundingRect(block).height();
+
+    while (block.isValid() && top <= event->rect().bottom()) {
+        if (block.isVisible() && bottom >= event->rect().top()) {
+            QString number = QString::number(blockNumber + 1);
+            painter.setPen(Qt::black);
+            painter.drawText(0, top, codeLineArea->width(), fontMetrics().height(),
+                             Qt::AlignRight, number);
+        }
+
+        block = block.next();
+        top = bottom;
+        bottom = top + (int) blockBoundingRect(block).height();
+        ++blockNumber;
+    }
+
 }
